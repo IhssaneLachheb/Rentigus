5d4157cc9254b003fa24d6fd6b075a5fc8e63abc
IhssaneLachheb <lachhabihssan@gmail.com>
2021-06-15 21:34:50 +0100

    first commit

---
+++ assets/js/jscode.js
@@ -0,0 +1,12 @@
+
+ /****** typing animation *******/
+
+ var typed = new Typed(".typing", {
+ 	strings: ["Le logement de vos rêves", "Juste pour Vous"],
+ 	typeSpeed: 100,
+ 	backSpeed: 60,
+ 	loop: true
+ });
+
+ 
+
