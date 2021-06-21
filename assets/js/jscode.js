510cf18077757d2ba3a18592e68695834547a716
IhssaneLachheb <35879713+IhssaneLachheb@users.noreply.github.com>
2021-06-21 15:23:32 +0100

    Update jscode.js

--- assets/js/jscode.js
+++ assets/js/jscode.js
@@ -2,7 +2,7 @@
  /****** typing animation *******/
 
  var typed = new Typed(".typing", {
- 	strings: ["Le logement de vos rêves", "Juste pour Vous"],
+ 	strings: ["La location de vos rêves", "Juste pour Vous"],
  	typeSpeed: 100,
  	backSpeed: 60,
  	loop: true
