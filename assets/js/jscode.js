e188c4be16557be8637f964ed3fd7545179e11eb
IhssaneLachheb <35879713+IhssaneLachheb@users.noreply.github.com>
2021-06-21 15:48:42 +0100

    Update jscode.js

--- assets/js/jscode.js
+++ assets/js/jscode.js
@@ -2,7 +2,7 @@
  /****** typing animation *******/
 
  var typed = new Typed(".typing", {
- 	strings: ["La location de vos rêves", "Notre service de location habitat", "Notre service de location professionnel"],
+ 	strings: ["La location de vos rêves", "Service de location habitat", "Service de location professionnel"],
  	typeSpeed: 100,
  	backSpeed: 60,
  	loop: true
